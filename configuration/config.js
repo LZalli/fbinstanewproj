f9452fe24c3320133b9bfc37592e64eb2bf0c690
LZalli <harzalli13@gmail.com>
2021-08-31 09:51:20 +0100

    commit 31 aout

--- configuration/config.js
+++ configuration/config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  /*
-   * This file contains the configurations information of Twitter login app.
-   * It consists of Twitter app information, database information.
-   */
+/*
+    * Ce fichier contient les informations de configuration de l'application de connexion Facebook.
+    */
 
   //  FB params
   facebook_api_key: "352614316345304",
   facebook_api_secret: "be44200611515e4026a66c8635be081c",
 
 
-  callback_url: "https://fbinstaproject.herokuapp.com/auth/facebook/callback/auth/facebook/callback",
+  callback_url: "https://fbinstaproject.herokuapp.com/auth/facebook/callback",
+
+ 
   use_database: false,
   host: "localhost",
   username: "root",

efb6445a49c5c27be9a4b67d3dd6d86e28e8ff7d
LZalli <harzalli13@gmail.com>
2021-10-08 13:44:36 +0100

    testing login

--- routes/facebookapi.js
+++ routes/facebookapi.js
@@ -1455,5 +1455,34 @@ router.get('/benchmark', function(req, res){
    });
 //end
 
+
+
+
+router.get('/alluser', function(req, res){
+   
+  
+
+  
+  MongoClient.connect('mongodb+srv://Laith:Azer1234@cluster0.9pyqc.mongodb.net/Data', (err, client) => {
+      // Client returned
+     var db = client.db('mytestingdb');
+     db.collection('admins').find({}).toArray(function(err, pagefansgenderage) {
+        // Print the documents returned
+        res.json(pagefansgenderage);
+  
+      /**  docs.forEach(function(doc) {
+          console.log(doc.name);
+        }); **/
+        // Close the DB
+        client.close();
+  
+        });
+
+  
+    }) 
+  
+   });
+
+
   module.exports = router;
   
